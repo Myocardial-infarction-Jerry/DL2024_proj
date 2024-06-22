18005f4cd7505b3252867ad04b868df0e58405d7
Myocardial-infarction-Jerry <liusy228@mail2.sysu.edu.cn>
2024-06-22 13:27:47 +0800

    ✨ feat(Code): Add progress bar

--- main_interpretability.py
+++ main_interpretability.py
@@ -1,13 +1,25 @@
 import os
 import numpy as np
+import torch
 import data_utils.transform as tr
 from config import INIT_TRAINER
 from torchvision import transforms
 from converter.common_utils import hdf5_reader
 from analysis.analysis_tools import calculate_CAMs, save_heatmap
 from data_utils.csv_reader import csv_reader_single
+from tqdm import tqdm
 
 mod = "ResNet1.0"
+
+# 将numpy数组转为PyTorch张量并转移到GPU
+
+
+def to_cuda(numpy_array):
+    tensor = torch.tensor(numpy_array)
+    if torch.cuda.is_available():
+        tensor = tensor.cuda()
+    return tensor
+
 
 test_csv_path = './csv_file/cub_200_2011.csv_test.csv'
 label_dict = csv_reader_single(test_csv_path, key_col='id', value_col='label')
@@ -23,13 +35,16 @@ features = []
 img_paths = []
 
 for i in range(len(item)):
-    features.append(hdf5_reader('./analysis/mid_feature/%s/fold1/%s'%(mod,item[i]),'feature_in'))
+    feature = hdf5_reader('./analysis/mid_feature/%s/fold1/%s' %
+                          (mod, item[i]), 'feature_in')
+    features.append(to_cuda(feature))
     # 默认的hook获取池化层的输入和输出，池化层的输入'feature_in'即为最后一层卷积层的输出
-    img_paths.append('./datasets/CUB_200_2011/CUB_200_2011/images/%s/%s.jpg'%(sort[i],item[i]))
+    img_paths.append(
+        './datasets/CUB_200_2011/CUB_200_2011/images/%s/%s.jpg' % (sort[i], item[i]))
     # 对应的原始图像路径
 
-
-weight = np.load('./analysis/result/%s/fold1_fc_weight.npy'%mod)
+weight = np.load('./analysis/result/%s/fold1_fc_weight.npy' % mod)
+weight = to_cuda(weight)  # 将权重转移到GPU
 # 线性层的权重
 transformer = transforms.Compose([
     tr.ToCVImage(),
@@ -39,10 +54,13 @@ transformer = transforms.Compose([
     tr.ToArray(),
 ])
 
-classes = 200 # 总类别数
-class_idx = 0 # 模型预测类别，也可以从最终结果的csv里面批量读取
-cam_path = './analysis/result/%s/cams/'%mod
+classes = 200  # 总类别数
+class_idx = 0  # 模型预测类别，也可以从最终结果的csv里面批量读取
+cam_path = './analysis/result/%s/cams/' % mod
 
-for i in range(len(item)):
-    cams = calculate_CAMs(features[i], weight, range(classes))  
-    save_heatmap(cams, img_paths[i], class_idx, cam_path, transform=transformer)
+for i in tqdm(range(len(item)), desc="Processing Items"):
+    cams = calculate_CAMs(features[i], weight, range(
+        classes))  # 确保calculate_CAMs支持CUDA张量
+    # 确保save_heatmap支持CUDA张量
+    save_heatmap(cams, img_paths[i], class_idx,
+                 cam_path, transform=transformer)
